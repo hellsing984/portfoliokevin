2d7dc758db94b795ec6b446c5f97eb6535d6f147
hellsing984 <kevin.sicre.dwwm48@gmail.com>
2025-01-16 10:36:44 +0100

    projet remis

---
+++ script.js
@@ -0,0 +1,15 @@
+    // code pour les bouton du footer
+    document.querySelectorAll('button').forEach(button => {
+      button.addEventListener('click', () => {
+        alert(`c'est bien   ${button.textContent}`);
+      });
+    });
+
+    document.querySelectorAll('.hover-highlight').forEach(card => {
+      card.addEventListener('mouseover', () => {
+        card.style.boxShadow = '0 4px 20px rgba(0, 0, 0, 0.2)';
+      });
+      card.addEventListener('mouseout', () => {
+        card.style.boxShadow = '0 1px 3px rgba(0, 0, 0, 0.1)';
+      });
+    });
