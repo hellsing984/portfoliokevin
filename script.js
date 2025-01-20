b46929e969c5747c9a8112a5edac28caf29b0506
hellsing984 <kevin.sicre.dwwm48@gmail.com>
2025-01-20 10:33:01 +0100

    mj du 20/01/2025

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     // code pour les bouton du footer
     document.querySelectorAll('button').forEach(button => {
       button.addEventListener('click', () => {
-        alert(`c'est bien   ${button.textContent}`);
+        alert(`    ${button.textContent}`);
       });
     });
 
@@ -13,7 +13,12 @@
         card.style.boxShadow = '0 1px 3px rgba(0, 0, 0, 0.1)';
       });
     });
-
-
-
-
+   // Sélection du logo et du menu
+   const logo = document.getElementById('logo');
+   const menu = document.getElementById('menu');
+ 
+   // Ajout d'un événement de clic sur le logo
+   logo.addEventListener('click', function() {
+     // Alterne la visibilité du menu
+     menu.classList.toggle('hidden');
+   });
