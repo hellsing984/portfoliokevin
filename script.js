00afc183ba5c79fa53fad3fc02017fd5830c10a9
hellsing984 <kevin.sicre.dwwm48@gmail.com>
2025-01-20 13:50:11 +0100

    mj 20/01 menu déroulant

--- script.js
+++ script.js
@@ -4,21 +4,12 @@
         alert(`    ${button.textContent}`);
       });
     });
-
-    document.querySelectorAll('.hover-highlight').forEach(card => {
-      card.addEventListener('mouseover', () => {
-        card.style.boxShadow = '0 4px 20px rgba(0, 0, 0, 0.2)';
+      // Sélection du logo et du menu
+      const logo = document.getElementById('Menu1');
+      const menu = document.getElementById('menu');
+    
+      // Ajout d'un événement de clic sur le logo
+      logo.addEventListener('click', function() {
+        // Alterne la visibilité du menu
+        menu.classList.toggle('hidden');
       });
-      card.addEventListener('mouseout', () => {
-        card.style.boxShadow = '0 1px 3px rgba(0, 0, 0, 0.1)';
-      });
-    });
-   // Sélection du logo et du menu
-   const logo = document.getElementById('logo');
-   const menu = document.getElementById('menu');
- 
-   // Ajout d'un événement de clic sur le logo
-   logo.addEventListener('click', function() {
-     // Alterne la visibilité du menu
-     menu.classList.toggle('hidden');
-   });
